832ae1de034725660fed8787af70fa1a4ecf30e8
Urmat <urmat@Urmat.local>
2019-07-05 12:46:15 +0600

    Added app-swoosh

---
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Urmat on 7/2/19.
+//  Copyright © 2019 Urmat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
